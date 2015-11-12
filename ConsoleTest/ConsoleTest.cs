9cf5b280db522cd82247e844d9a54a89164e3449
minonastya <minovskayanastya@gmail.com>
2015-11-12 23:16:41 +0300

    Commit

---
+++ ConsoleTest/ConsoleTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OurLib;
+
+namespace ConsoleApplication1
+{
+    class ConsoleTest
+    {
+        static void Main(string[] args)
+        {
+            Tuple<double, double> a = new Tuple<double, double>(0.0, 0.0);
+            Tuple<double, double> b = new Tuple<double, double>(5.0, 5.0);
+            Tuple<double, double> c = new Tuple<double, double>(0.0, 0.0);
+            Tuple<double, double> d = new Tuple<double, double>(5.0, 0.0);
+            Console.WriteLine(TwoLines.IsIntersect(a, b, c, d) ? "DA" : "NET");        
+        }
+    }
+}
---
+++ OurLib/TwoLines.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OurLib
+{
+    public class TwoLines
+    {
+        public static bool IsIntersect(Tuple<double, double> a, Tuple<double, double> b, Tuple<double, double> c, Tuple<double, double> d)
+        {
+            double v1, v2, v3, v4;
+            if ((a.Item1 == c.Item1 && a.Item2 == c.Item2) || (b.Item1 == d.Item1 && b.Item2 == d.Item2) ||
+                (a.Item1 == d.Item1 && a.Item2 == d.Item2) || (b.Item1 == c.Item1 && b.Item2 == c.Item2)) return true;
+            v1 = (d.Item1 - c.Item1) * (a.Item2 - c.Item2) - (d.Item2 - c.Item2) * (a.Item1 - c.Item1);
+            v2 = (d.Item1 - c.Item1) * (b.Item2 - c.Item2) - (d.Item2 - c.Item2) * (b.Item1 - c.Item1);
+            v3 = (b.Item1 - a.Item1) * (c.Item2 - a.Item2) - (b.Item2 - a.Item2) * (c.Item1 - a.Item1);
+            v4 = (b.Item1 - a.Item1) * (d.Item2 - a.Item2) - (b.Item2 - a.Item2) * (d.Item1 - a.Item1);
+            bool res = v1 * v2 < 0 && v3 * v4 < 0;
+            return res;
+        }
+    }
+}
